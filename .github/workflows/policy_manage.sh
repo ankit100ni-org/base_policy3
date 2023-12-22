ca0a7f394c2b0b9ae754a8484865741c1bf57421
Ankit Soni <ankit.soni@chef.io>
2023-12-22 11:36:19 +0530

    whole testing

--- .github/workflows/policy_manage.sh
+++ .github/workflows/policy_manage.sh
@@ -32,10 +32,7 @@ echo "$json_data" | jq -r 'to_entries[] | "\(.key) \(.value.client_name) \(.valu
     echo "knife connectivity is failed for org $org_name"
     echo "knife connectivity is failed for org $org_name" >> "$failed_org_log"
   fi
-  ls -lhrt 
-  sudo apt install expect
-  sudo chmod 777 .github/workflows/accept_cert.sh
-  sudo bash .github/workflows/accept_cert.sh
+
   chef show-policy
 done
 
